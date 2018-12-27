69b9e15fbd75ffd79c7812ccc083adb8f154adac
Mikolla <miqolay@gmail.com>
2018-12-27 20:11:03 +0500

    config spring boot clean update

--- src/main/java/ru/springbootstrap/config/security/SecurityConfig.java
+++ src/main/java/ru/springbootstrap/config/security/SecurityConfig.java
@@ -1,12 +1,9 @@
 package ru.springbootstrap.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -16,9 +13,6 @@ import ru.springbootstrap.config.security.service.AuthenticationService;
 
 
 @Configuration
-@ComponentScan("ru.springbootstrap")
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthenticationService authenticationService;
