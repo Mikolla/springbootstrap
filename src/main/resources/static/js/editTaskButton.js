62c8efdb18000209e589bc85f2d9a87ba3f0af32
Mikolla <miqolay@gmail.com>
2019-10-25 19:07:08 +0500

    task editor form fill and read

--- src/main/resources/static/js/editTaskButton.js
+++ src/main/resources/static/js/editTaskButton.js
@@ -31,9 +31,28 @@ $(document).on("click", '#editTaskButton', function() {
 
     var idForm = $('<div class="form-group"></div>');
 
+    idForm.append('<label for="id" style="margin-top: 5%">id</label>');
+    idForm.append('<input class="form-control"  disabled="disabled"  name="id" id="id" value="'+taskId+'" />');
+
+    idForm.append('<input class="form-control"  type="hidden"  name="id" id="id" value="'+taskId+'" ref="id"/>');
 
 
+    idForm.append('<label for="'+columnHeadings[1]+'" style="margin-top: 5%">title</label>');
+    idForm.append('<input class="form-control" name="title" id="title" value="'+taskTitle+'" ref="title"/>');
 
+
+    var sel = $('<select class="form-control" name="taskState" id="taskState"></select>');
+    if (renderState == 'true') {
+        sel.append('<option value="true">true</option>');
+        sel.append('<option value="false">false</option>');
+    } else {
+        sel.append('<option value="false">false</option>');
+        sel.append('<option value="true">true</option>');
+    }
+
+
+    idForm.append('<label for="state" style="margin-top: 5%">state</label>')
+    idForm.append(sel);
 
     modalForm.append(idForm);
 
@@ -44,6 +63,32 @@ $(document).on("click", '#editTaskButton', function() {
 
     /* modal form block */
 
+
+});
+
+
+
+
+
+
+
+
+$(document).on("click", '#saveTaskButton', function() {
+    console.log('save button clicked')
+
+    var myform = document.getElementById("formoid");
+    var formData = new FormData(myform);
+
+    var queryString = $('#formoid').serialize();
+    console.log(queryString);
+
+
+    let wrap = {
+        taskId: formData.get('id'),
+        taskTitle: formData.get('title'),
+        taskState: formData.get('taskState')
+    };
+    let data = JSON.stringify(wrap);
 
 
 
